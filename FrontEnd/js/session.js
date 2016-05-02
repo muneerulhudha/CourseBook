908766baaefecdcf7757f090bde6c6c2aae352fa
Muneer Hudha <muneerulhudha@gmail.com>
2016-05-02 11:37:09 -0500

    Changed everything

--- FrontEnd/js/session.js
+++ FrontEnd/js/session.js
@@ -1,8 +1,61 @@
+function addCart(courseNumber){
+	console.log("in Add cart");
+	
+	var x = document.cookie;
+	var user = x.split("=")[1];
+	
+	var json = {
+		"username": user,
+		"courseno": courseNumber
+	};
+	
+	$.ajax({
+      type: "POST",
+      data: json,
+      headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+      url: "rest/cart",
+      success: function(result){
+          console.log(result);
+          if(result.success == "true"){
+        	  //console.log("swal");
+        	  swal("Added to Cart!");
+        	  //$("#reviewForm")[0].reset();
+          }
+      }
+    });
+}
+
 jQuery(document).ready(function($) {
 
 	$("#logout").click(function(){
 		document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
 		window.location.replace("index.html");
+	});
+	
+	$("#checkoutButton").click(function(){
+		
+		var x = document.cookie;
+    	var user = x.split("=")[1];
+		
+    	var json = {
+       			"username": user
+        };
+    	
+		$.ajax({
+	      type: "POST",
+	      data: json,
+	      headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+	      url: "rest/checkout",
+	      success: function(result){
+	          console.log(result);
+	          if(result.success == "true"){
+	        	  //console.log("swal");
+	        	 // swal("Review added successfully!");
+	        	  window.location.replace("history.html");
+	          }
+	      }
+	    });
+		    
 	});
 	
 	$("#searchForm").submit(function(e){
@@ -29,7 +82,7 @@ jQuery(document).ready(function($) {
                     "<td><p class = \"classNumber\">" + value.classNumber + "</p></td>"+
                     "<td><p class = \"classSection\">" + value.classSection + "</p></td>"+
                     "<td><p class = \"profName\">" + value.profName + "</p></td>"+
-                    "<td><button type=\"button\" class=\"btn-cart\">Add to cart!</button><td>"; 
+                    "<td><button onclick=\"addCart('"+ value.classNumber +"')\" type=\"button\" class=\"btn-cart\">Add to cart!</button><td>"; 
                 // htmlAppend += "<td><button type=\"button\">Add to cart!</button><td>";             
             });
 	        htmlAppend+="</tbody></table></div>";
@@ -44,6 +97,27 @@ jQuery(document).ready(function($) {
 		e.preventDefault();
 		return false;
 		
+	});
+	
+	$("#reviewForm").submit(function(e){
+
+	    $.ajax({
+	      type: "POST",
+	      data: $("#reviewForm").serialize(),
+	      headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+	      url: "rest/review",
+	      success: function(result){
+	          console.log(result);
+	          if(result.success == "true"){
+	        	  //console.log("swal");
+	        	  swal("Review added successfully!");
+	        	  $("#reviewForm")[0].reset();
+	          }
+	      }
+	    });
+	    
+	    e.preventDefault();
+	    return false;
 	});
 	
 	$("#profileForm").submit(function(e){
